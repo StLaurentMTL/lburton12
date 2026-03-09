8487a1a8a0df062fbaa7119f56701079a0b50353
StLaurentMTL <95014337+StLaurentMTL@users.noreply.github.com>
2026-03-09 23:26:40 +0000

    Deploying to gh-pages from @ StLaurentMTL/lburton12@a2f480a1221d090df2fb90fe27940db36e381964 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,9 +402,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/lburton12/projects/1_project/";
             },},{id: "projects-project-2",
