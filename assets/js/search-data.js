8b7395f9e37f6f6cd632761ac621c77ed6692227
StLaurentMTL <95014337+StLaurentMTL@users.noreply.github.com>
2026-03-10 00:18:45 +0000

    Deploying to gh-pages from @ StLaurentMTL/lburton12@9a2e839e367b0e31f8c02069b8540bfa0f0b6b9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,11 +412,6 @@ ninja.data = [{
           description: "A Computational Analysis of President Xi Jin Ping&#39;s (习近平) speeches",
           section: "Projects",handler: () => {
               window.location.href = "/lburton12/projects/2_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/lburton12/projects/4_project/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
